fe91a2cdb9519ade3bd8d872c6f96c6e71dba231
wlnhoge <georg.hoeller@raiffeisenbank.at>
2023-06-05 19:11:32 +0200

    udpate

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const pug = require('pug');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+const { ObjectID } = require('mongodb');
+
 
 const app = express();
 
@@ -15,8 +23,46 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('index',{title:'Hello',message:'Please log in'})
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+passport.initialize() 
+passport.session()
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+  });
+});
+
+
+myDB(async client => {
+  // const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization here...
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
